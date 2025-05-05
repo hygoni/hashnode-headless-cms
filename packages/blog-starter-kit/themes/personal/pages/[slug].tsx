545a485bb7e41f343cb6f79ea34087d5f9d89e04
hyeyoo <42.hyeyoo@gmail.com>
2025-05-05 14:21:55 +0900

    Update [slug].tsx

--- packages/blog-starter-kit/themes/personal/pages/[slug].tsx
+++ packages/blog-starter-kit/themes/personal/pages/[slug].tsx
@@ -171,7 +171,7 @@ export default function PostOrPage(props: Props) {
 	return (
 		<AppProvider publication={publication} post={maybePost} page={maybePage}>
 			<Layout>
-				<Container className="mx-auto flex max-w-5xl flex-col items-stretch gap-10 px-5 py-10">
+				<Container className="mx-auto flex max-w-6xl flex-col items-stretch gap-10 px-5 py-10">
 					<PersonalHeader />
 					<article className="flex flex-col items-start gap-10 pb-10">
 						{props.type === 'post' && <Post {...props} />}
